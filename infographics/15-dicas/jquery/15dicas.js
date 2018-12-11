1b83af25e950f84a8dbb1c3ea0239e5304774383
André Tamane <nobisa212@gmail.com>
2018-12-11 10:56:35 -0200

    Add files via upload

---
+++ infographics/15-dicas/jquery/15dicas.js
@@ -0,0 +1,11 @@
+// JavaScript Document
+
+$(function() {
+    $('a').bind('click',function(event){
+        var $anchor = $(this);
+		$('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500,'easeInOutExpo');
+        event.preventDefault();
+    });	
+});
